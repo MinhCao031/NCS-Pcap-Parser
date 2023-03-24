1d74ca862c259977ab7b6e47733e1efbb4e83c8d
Lucifer-02 <lucifer.02.nice@gmail.com>
2023-03-24 09:27:51 +0700

    push new base

--- test.c
+++ test.c
@@ -26,7 +26,6 @@ static const struct {
 };
 #define NCOMMANDS (sizeof commands / sizeof commands[0])
 
-
 /*
  * A CMD is an SMTP command, MESSAGE is the message portion, and EOM is the
  * last part of a message
@@ -222,7 +221,7 @@ gint tvb_strneql(const u_char *tvb, const gint offset, const gchar *str,
   }
 }
 int smtp_decoder(u_char const *tvb, gint tvb_size,
-                  struct smtp_session_state *session_state, gboolean request) {
+                 struct smtp_session_state *session_state, gboolean request) {
 
   struct smtp_proto_data *spd_frame_data;
   int offset = 0;
@@ -507,7 +506,7 @@ void flow_browser(flow_base_t *flow) {
 }
 
 void get_packets(pcap_t *handler, FILE *fout_parser, FILE *fout_seq_filter,
-                 FILE *fout_list_flow) ;
+                 FILE *fout_list_flow);
 int main(void) {
   // error buffer
   char errbuff[PCAP_ERRBUF_SIZE];
@@ -531,68 +530,56 @@ int main(void) {
   return 0;
 }
 
-void get_packets(pcap_t *handler, FILE* fout_parser, FILE* fout_seq_filter, FILE* fout_list_flow) {
+void get_packets(pcap_t *handler, FILE *fout_parser, FILE *fout_seq_filter,
+                 FILE *fout_list_flow) {
 
   // The header that pcap gives us
   struct pcap_pkthdr *header_pcap;
 
   // The actual packet
-  u_char const* full_packet;
-
-  struct timespec pkt_start, pkt_end;
-  uint64_t process_time = 0;
-  uint64_t process_time_total = 0;
+  u_char const *full_packet;
 
   // create hash table
   HashTable table = create_hash_table(HASH_TABLE_SIZE);
 
   while (pcap_next_ex(handler, &header_pcap, &full_packet) >= 0) {
 
-    // Show the packet number & timestamp
-    GET_FULL_TIMESTAMP;
     captured_packets++;
     // printf("#%d\n", ++captured_packets);
 
-    LOG_DBG(fout_parser, DBG_PARSER,
-      "Packet # %i\nTime in sec & microsec: %lu.%7lu\nFull timestamp = %s\n",
-      captured_packets, (header_pcap->ts).tv_sec, (header_pcap->ts).tv_usec, full_timestamp
-    );
-
-    clock_gettime(CLOCK_REALTIME, &pkt_start);
-
     int8_t progress_pkt = 1;
-    // Dissection Step 1 of 4----------------------------------------------------------------------
+    // Dissection Step 1 of
+    // 4----------------------------------------------------------------------
     package frame = frame_dissector(full_packet, header_pcap, fout_parser);
     if (frame.is_valid == false) {
       LOG_DBG(fout_parser, DBG_PARSER, "ERROR: Frame is not valid!\n");
-      clock_gettime(CLOCK_REALTIME, &pkt_end);
       goto END;
     }
 
     progress_pkt += 1;
-    // Dissection Step 2 of 4----------------------------------------------------------------------
+    // Dissection Step 2 of
+    // 4----------------------------------------------------------------------
     package packet = link_dissector(frame, fout_parser);
     if (packet.is_valid == false) {
       LOG_DBG(fout_parser, DBG_PARSER, "ERROR: Packet is not valid!\n");
-      clock_gettime(CLOCK_REALTIME, &pkt_end);
       goto END;
     }
 
     progress_pkt += 1;
-    // Dissection Step 3 of 4----------------------------------------------------------------------
+    // Dissection Step 3 of
+    // 4----------------------------------------------------------------------
     package segment = network_dissector(packet, fout_parser);
     if (segment.is_valid == false) {
       LOG_DBG(fout_parser, DBG_PARSER, "ERROR: Segment is not valid!\n");
-      clock_gettime(CLOCK_REALTIME, &pkt_end);
       goto END;
     }
 
     progress_pkt += 1;
-    // Dissection Step 4 of 4----------------------------------------------------------------------
+    // Dissection Step 4 of
+    // 4----------------------------------------------------------------------
     package payload = transport_demux(segment, fout_parser);
     if (payload.is_valid == false) {
       LOG_DBG(fout_parser, DBG_PARSER, "ERROR: Payload is not valid!\n");
-      clock_gettime(CLOCK_REALTIME, &pkt_end);
       goto END;
     }
 
@@ -601,52 +588,36 @@ void get_packets(pcap_t *handler, FILE* fout_parser, FILE* fout_seq_filter, FILE
     parsed_packet pkt = pkt_parser(packet, segment, payload);
 
     insert_packet(table, pkt, fout_parser);
-    clock_gettime(CLOCK_REALTIME, &pkt_end);
-
-    printf(
-      "Tracking #%-3u SEQ = %10u => %10u, ACK = %10u\n", captured_packets,
-      pkt.tcp.seq, pkt.tcp.seq + pkt.payload.data_len, pkt.tcp.ack_seq
-    );
 
     progress_pkt += 1;
-    PROCESS_PACKET_TIME(50000);
     LOG_DBG(fout_parser, DBG_PARSER,
-      "----------------------------------------"
-      "-----------Successfully---------------\n");
-    if (captured_packets > LIMIT_PACKET) break;
+            "----------------------------------------"
+            "-----------Successfully---------------\n");
+    if (captured_packets > LIMIT_PACKET)
+      break;
     continue;
 
-    END: {
-      PROCESS_PACKET_TIME(50000);
-      LOG_DBG(fout_parser, DBG_PARSER,
-        "----------------------------------------"
-        "-----------PacketFailed---------------\n");
-      if (captured_packets > LIMIT_PACKET) break;
-    }
+  END : {
+    LOG_DBG(fout_parser, DBG_PARSER,
+            "----------------------------------------"
+            "-----------PacketFailed---------------\n");
+    if (captured_packets > LIMIT_PACKET)
+      break;
   }
-
-  STATISTIC_PACKET_TIME;
+  }
 
   // Print HashTable
   print_hashtable(table, fout_list_flow);
 
-  // Test a random flow
-  // printf("\nTest 01: Get a random flow\n");
-
-  // flow_base_t* flow_test = search_flow(table, 6813568831684183325, stdout);
-  // if (flow_test) {
-  //   print_flow(*flow_test, stdout);
-  //   printf("\nTest 02: Get payloads in flow\n");
-  //   // char* long_payload = payload_to_string(flow_test->head_flow, flow_test->total_payload);
-  //   // printf("All payload in this flow:\n%s\n\n<END OF FLOW>\n", long_payload);
-  // } else printf("Flow not found.\n");
+  flow_base_t *flow_test = search_flow(table, 6813568831684183325, stdout);
+  flow_browser(flow_test);
 
   LOG_DBG(fout_list_flow, DBG_FLOW,
-    "Number of packets: %u\nNumber of flows: %u\n"
-    "Number of inserted packets: %u\nNumber of filtered packets: %u\n",
-    captured_packets, count_flows(table), inserted_packets, filtered_packets
-  );
+          "Number of packets: %u\nNumber of flows: %u\n"
+          "Number of inserted packets: %u\nNumber of filtered packets: %u\n",
+          captured_packets, count_flows(table), inserted_packets,
+          filtered_packets);
 
-  printf("\nTest 03: Freeing...\n");
-  // free_hash_table(table);
+  printf("\nFreeing...\n");
+  free_hash_table(table);
 }
